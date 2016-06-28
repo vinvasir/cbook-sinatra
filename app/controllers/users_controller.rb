99612d472094e83f3473d88aebbe8580a815acea
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-28 17:12:00 +0000

    Created the registration form and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :'/users/new'
   end
+  
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      redirect '/signup'
+    end
+  end
 end
