a95e2c0e7e60e18891f415e0334f78cb3335a97a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-30 17:48:22 +0000

    Filled in the correct values for each star input on the review form. Added hidden input for user_id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,6 @@
-require 'pry'
 class ReviewsController < ApplicationController
   post '/reviews/:comic_book_id' do
     @comic_book = ComicBook.find(params[:comic_book_id])
-    binding.pry
     redirect "/comic_books/#{@comic_book.id}"
   end    
 end
