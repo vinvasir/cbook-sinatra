a9a78f90e77adbf9ef5f84710cfcaef06f0b28c9
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-30 17:59:53 +0000

    Created the controller action for adding reviews to a comic book. Displayed each review's author on the show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,13 @@
 class ReviewsController < ApplicationController
   post '/reviews/:comic_book_id' do
-    @comic_book = ComicBook.find(params[:comic_book_id])
-    redirect "/comic_books/#{@comic_book.id}"
+    if session[:user_id]
+      @comic_book = ComicBook.find(params[:comic_book_id])
+      @comic_book.reviews.create(content: params["content"], 
+                                  rating: params["rating"],
+                                  user_id: params["user_id"])
+      redirect "/comic_books/#{@comic_book.id}"
+    else
+      redirect '/login'
+    end
   end    
 end
