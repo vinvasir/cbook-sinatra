41867aaea64bb6106d63be999f085216402e1593
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-30 20:25:16 +0000

    Created new author form with post logic and validations.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,4 +3,22 @@ class AuthorsController < ApplicationController
     @authors = Author.all
     erb :'/authors/index'
   end
+  
+  get '/authors/new' do
+    if session[:user_id]
+      erb :'/authors/new'
+    else
+      redirect '/login'
+    end
+  end
+  
+  post '/authors' do
+    @author = Author.new(params)
+    if @author.save
+      redirect '/authors'
+    else
+      flash[:error] = "You must enter an author name!"
+      redirect '/authors/new'
+    end
+  end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ActiveRecord::Base
   has_many :comic_books
+  validates_presence_of :name
 end
