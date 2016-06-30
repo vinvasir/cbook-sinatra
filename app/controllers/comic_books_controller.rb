cd96c416ee5cc5e2d4aa18b7341a66cc7f5ebd8e
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-30 16:52:39 +0000

    Wrote the comic_books CREATE logic with redirect to the show page. Made a basic show page.

--- app/controllers/comic_books_controller.rb
+++ app/controllers/comic_books_controller.rb
@@ -7,4 +7,14 @@ class ComicBooksController < ApplicationController
   get '/comic_books/new' do
     erb :'/comic_books/new'
   end
+  
+  get '/comic_books/:id' do
+    @comic_book = ComicBook.find(params[:id])
+    erb :'/comic_books/show'
+  end
+  
+  post '/comic_books' do
+    @comic_book = ComicBook.create(params)
+    redirect "/comic_books/#{@comic_book.id}"
+  end
 end
