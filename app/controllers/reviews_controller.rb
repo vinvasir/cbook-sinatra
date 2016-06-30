b5f9ca0fded570cb605177ab7f7d051db54f1b2a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-30 18:36:00 +0000

    Added review validations and flash messages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,18 @@ class ReviewsController < ApplicationController
   post '/reviews/:comic_book_id' do
     if session[:user_id]
       @comic_book = ComicBook.find(params[:comic_book_id])
-      @comic_book.reviews.create(content: params["content"], 
+      review = @comic_book.reviews.new(content: params["content"], 
                                   rating: params["rating"],
                                   user_id: params["user_id"])
-      redirect "/comic_books/#{@comic_book.id}"
+      if review.save
+        flash[:message] = "Successfully added review."
+        redirect "/comic_books/#{@comic_book.id}"
+      else
+        flash[:message] = "Please enter both a review and rating."
+        redirect "/comic_books/#{@comic_book.id}"
+      end
     else
+      flash[:message] = "You need to be logged in to add a review!"
       redirect '/login'
     end
   end    
